a1dd97facc105db35f687568d7ecf15878ad9db9
Adian Latifa Nurrohman <Adian Latifa Nurrohman>
2016-12-18 22:40:32 +0700

    Menambahkan list kontak

--- Android/bromes/app/src/main/java/com/example/nurro/bromes/AddContactFragment.java
+++ Android/bromes/app/src/main/java/com/example/nurro/bromes/AddContactFragment.java
@@ -1,11 +1,25 @@
 package com.example.nurro.bromes;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by nurro on 12/18/2016.
@@ -13,15 +27,79 @@ import android.view.ViewGroup;
 
 public class AddContactFragment extends Fragment{
     View myView;
+    Button saveButton;
+    String no;
+    String name;
+    String email;
+    String address;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.add_contact, container, false);
+        initUI();
         return myView;
     }
     public void onBackPressed() {
         if(getFragmentManager().getBackStackEntryCount() > 0) {
             getFragmentManager().popBackStack();
         }
+    }
+
+    public void initUI(){
+        saveButton = (Button) myView.findViewById(R.id.saveNewContactButton);
+        saveButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textViewNo = (TextView) getActivity().findViewById(R.id.addNumber);
+                TextView textViewName = (TextView) getActivity().findViewById(R.id.addName);
+                TextView textViewEmail = (TextView) getActivity().findViewById(R.id.addEmail);
+                TextView textViewAddress = (TextView) getActivity().findViewById(R.id.addAddress);
+
+                no = textViewNo.getText().toString();
+                name = textViewName.getText().toString();
+                email = textViewEmail.getText().toString();
+                address = textViewAddress.getText().toString();
+
+                if (no.equals("") || name.equals("")) {
+                    Toast.makeText(v.getContext(), "Data kurang lengkap", Toast.LENGTH_SHORT).show();
+                } else {
+                    String link = "http://10.151.43.169/bromes/php/android_insert.php?id=add_contact&no="+no+
+                            "&name="+name+"&email="+email+"&address="+address;
+                    Log.d("Link", link.toString());
+                    boolean result = addContact(link);
+                    if(result){
+                        onBackPressed();
+                        FragmentManager fragmentManager = getFragmentManager();
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.contacts, new ContactFragment())
+                                .commit();
+                    }
+                }
+            }
+        });
+    }
+
+    public boolean addContact(String link){
+        boolean result = false;
+        try{
+            URLConnection url = new URL(link).openConnection();
+            HttpURLConnection con = (HttpURLConnection)url;
+            InputStream inputStream = new BufferedInputStream(con.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            int res = Integer.parseInt(br.readLine());
+
+            if(res == 1){
+                Toast.makeText(this.getActivity(), "Data berhasil dimasukkan", Toast.LENGTH_SHORT).show();
+                result = true;
+            }
+            else
+                Toast.makeText(this.getActivity(), "Data gagal dimasukkan", Toast.LENGTH_SHORT).show();
+
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return result;
     }
 }
--- Android/bromes/app/src/main/java/com/example/nurro/bromes/ContactFragment.java
+++ Android/bromes/app/src/main/java/com/example/nurro/bromes/ContactFragment.java
@@ -3,13 +3,29 @@ package com.example.nurro.bromes;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by nurro on 12/18/2016.
@@ -17,12 +33,26 @@ import android.view.ViewGroup;
 
 public class ContactFragment extends Fragment{
     View myView;
+    ListView myList;
+    ArrayAdapter<String> adapter;
+    InputStream inputStream = null;
+    String link = "http://10.151.43.169/bromes/php/android_search.php?id=contacts";
+    String line = null;
+    String result = null;
+    String[] data;
     @Nullable
     @Override
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         myView = inflater.inflate(R.layout.contacts, container, false);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        getData();
+
+        adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, data);
+
+        myList.setAdapter(adapter);
+
         return myView;
     }
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -46,6 +76,43 @@ public class ContactFragment extends Fragment{
     public void onBackPressed() {
         if(getFragmentManager().getBackStackEntryCount() > 0) {
             getFragmentManager().popBackStack();
+        }
+    }
+    private void getData(){
+        myList = (ListView) myView.findViewById(R.id.contact_list);
+        try {
+            URLConnection url = new URL(link).openConnection();
+
+            HttpURLConnection con = (HttpURLConnection)url;
+            //con.setRequestMethod("GET");
+
+            inputStream = new BufferedInputStream(con.getInputStream());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder sb = new StringBuilder();
+
+            while((line = br.readLine()) != null){
+                sb.append(line+"\n");
+            }
+            inputStream.close();
+            result = sb.toString();
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        try {
+            JSONArray jsonArray = new JSONArray(result);
+            data = new String[jsonArray.length()];
+            for(int i = 0; i < jsonArray.length(); i++){
+                data[i] = jsonArray.getJSONObject(i).getString("name");
+                data[i] += "\n\t"+jsonArray.getJSONObject(i).getString("no");
+            }
+        }
+
+        catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 }
